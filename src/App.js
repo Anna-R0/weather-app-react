fc25b3a3dda35297f3232fb8c9dd0aac8d91a6ae
Anna <eviltwin007@yandex.ru>
2022-10-05 11:23:16 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Anna Ro and is
-        <a href="https://github.com/Anna-R0/weather-app-react" target="_blank">
-          {" "}
-          open-sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Anna Ro and is
+          <a
+            href="https://github.com/Anna-R0/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
