1f379ec70fd372b5dd0b542a0982e9633e823f50
Anna <eviltwin007@yandex.ru>
2022-10-05 12:13:00 -0700

    Edited HTML

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,15 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
+
       <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />{" "}
+          6°C{" "}
+        </div>
         <div className="col-6">
           <ul>
             <li>Precipitation:15%</li>
